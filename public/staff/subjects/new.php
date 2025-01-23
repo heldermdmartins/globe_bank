b6e8e7fcc068f81d2c76d45f9c89d3310e370067
Helder Martins <77513222+heldermdmartins@users.noreply.github.com>
2025-01-23 14:19:51 +0000

    added form action

--- public/staff/subjects/new.php
+++ public/staff/subjects/new.php
@@ -4,11 +4,11 @@ require_once('../../../private/initialize.php');
 
 $test = $_GET['test'] ?? '';
 
-if($test == '404') {
+if ($test == '404') {
   error_404();
-} elseif($test == '500') {
+} elseif ($test == '500') {
   error_500();
-} elseif($test == 'redirect') {
+} elseif ($test == 'redirect') {
   redirect_to(url_for('/staff/subjects/index.php'));
 }
 ?>
@@ -23,7 +23,7 @@ if($test == '404') {
   <div class="subject new">
     <h1>Create Subject</h1>
 
-    <form action="" method="post">
+    <form action="<?php echo url_for('/staff/subjects/create.php'); ?>" method="post">
       <dl>
         <dt>Menu Name</dt>
         <dd><input type="text" name="menu_name" value="" /></dd>
