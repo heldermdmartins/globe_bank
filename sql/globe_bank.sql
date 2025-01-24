ba4956a2808aacacd3fad56a00310eec42fe64b9
Helder Martins <77513222+heldermdmartins@users.noreply.github.com>
2025-01-24 12:13:18 +0000

    Added table pages

--- sql/globe_bank.sql
+++ sql/globe_bank.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Jan 23, 2025 at 09:05 PM
+-- Generation Time: Jan 24, 2025 at 12:09 PM
 -- Server version: 5.7.39
 -- PHP Version: 8.2.0
 
@@ -11,15 +11,40 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
 --
 -- Database: `globe_bank`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pages`
+--
+
+CREATE TABLE `pages` (
+  `id` int(11) NOT NULL,
+  `subject_id` int(11) DEFAULT NULL,
+  `menu_name` varchar(255) DEFAULT NULL,
+  `position` int(3) DEFAULT NULL,
+  `visible` tinyint(1) DEFAULT NULL,
+  `content` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `pages`
+--
+
+INSERT INTO `pages` (`id`, `subject_id`, `menu_name`, `position`, `visible`, `content`) VALUES
+(1, 1, 'Globe Bank', 1, 1, NULL),
+(2, 1, 'History', 2, 1, NULL),
+(3, 1, 'Leadership', 3, 1, NULL),
+(4, 1, 'Contact Us', 4, 1, NULL),
+(5, 2, 'Banking', 1, 1, NULL),
+(6, 2, 'Credit Cards', 2, 1, NULL),
+(7, 2, 'Mortgages', 3, 1, NULL),
+(8, 3, 'Checking', 1, 1, NULL),
+(9, 3, 'Loans', 2, 1, NULL),
+(10, 3, 'Merchant Services', 3, 1, NULL);
 
 -- --------------------------------------------------------
 
@@ -35,8 +60,24 @@ CREATE TABLE `subjects` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
+-- Dumping data for table `subjects`
+--
+
+INSERT INTO `subjects` (`id`, `menu_name`, `position`, `visible`) VALUES
+(1, 'About Globe Bank', 1, 1),
+(2, 'Consumer', 2, 1),
+(3, 'Small Business', 3, 0);
+
+--
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `pages`
+--
+ALTER TABLE `pages`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_subject_id` (`subject_id`);
 
 --
 -- Indexes for table `subjects`
@@ -49,12 +90,14 @@ ALTER TABLE `subjects`
 --
 
 --
+-- AUTO_INCREMENT for table `pages`
+--
+ALTER TABLE `pages`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
 -- AUTO_INCREMENT for table `subjects`
 --
 ALTER TABLE `subjects`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
