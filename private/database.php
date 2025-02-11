4849a3a7767c8f45a3917bfcb617ab1d4c64fcce
Helder Martins <77513222+heldermdmartins@users.noreply.github.com>
2025-02-11 10:55:56 +0000

    add database

--- private/database.php
+++ private/database.php
@@ -12,7 +12,6 @@ function db_connect()
   $connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
   return $connection;
 }
-
 function db_disconnect($connection)
 {
   if (isset($connection)) {
