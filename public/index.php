708b529224c5714f2971c578ec00de7eef9c8336
Helder Martins <77513222+heldermdmartins@users.noreply.github.com>
2025-02-17 22:11:19 +0000

    Refactor URL handling and error functions; enhance code readability and maintainability. Add static homepage content and improve navigation links.

--- public/index.php
+++ public/index.php
@@ -1,10 +1,40 @@
 <?php require_once('../private/initialize.php'); ?>
+<?php
+if (isset($_GET['id'])) {
+  $page_id = $_GET['id'];
+  $page = find_page_by_id($page_id);
+  if (!$page) {
 
+    redirect_to(url_for('/index.php'));
+  }
+} else {
+  // Nothing selected; show the homepage
+}
+?>
 <?php include(SHARED_PATH . '/public_header.php'); ?>
 
 <div id="main">
+
   <?php include(SHARED_PATH . '/public_navigation.php'); ?>
+
   <div id="page">
+
+    <?php
+
+    if (isset($page)) {
+      // show the page from the database
+      // TODO add code to display the page
+      // be sure to escape the output
+      echo $page['content'];
+    } else {
+      // Show the homepage
+      // The homepage content could:
+      // * be static content (here or in a shared file)
+      // * show the first page from the nav
+      // * be in the database but add code to hide in the nav
+      include(SHARED_PATH . '/static_homepage.php');
+    }
+    ?>
 
   </div>
 
