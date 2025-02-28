45502e093432fa6b34d5028dc3fa57b14467655d
Helder Martins <77513222+heldermdmartins@users.noreply.github.com>
2025-02-28 21:12:40 +0000

    Refactor page and subject retrieval logic to include visibility checks; enhance error handling and maintainability

--- public/index.php
+++ public/index.php
@@ -1,20 +1,28 @@
 <?php require_once('../private/initialize.php'); ?>
 
 <?php
-if (isset($_GET['id'])) {
+if(isset($_GET['id'])) {
   $page_id = $_GET['id'];
-  $page = find_page_by_id($page_id);
-  if (!$page) {
+  $page = find_page_by_id($page_id, ['visible' => true]);
+  if(!$page) {
     redirect_to(url_for('/index.php'));
   }
   $subject_id = $page['subject_id'];
-} elseif (isset($_GET['subject_id'])) {
-  $subject_id = $_GET['subject_id'];
+  $subject = find_subject_by_id($subject_id, ['visible' => true]);
+  if(!$subject) {
+    redirect_to(url_for('/index.php'));
+  }
 
+} elseif(isset($_GET['subject_id'])) {
+  $subject_id = $_GET['subject_id'];
+  $subject = find_subject_by_id($subject_id, ['visible' => true]);
+  if(!$subject) {
+    redirect_to(url_for('/index.php'));
+  }
   $page_set = find_pages_by_subject_id($subject_id, ['visible' => true]);
   $page = mysqli_fetch_assoc($page_set); // first page
   mysqli_free_result($page_set);
-  if (!$page) {
+  if(!$page) {
     redirect_to(url_for('/index.php'));
   }
   $page_id = $page['id'];
@@ -33,18 +41,19 @@ if (isset($_GET['id'])) {
   <div id="page">
 
     <?php
-    if (isset($page)) {
-      // show the page from the database
-      // TODO add html escaping back in
-      echo $page['content'];
-    } else {
-      // Show the homepage
-      // The homepage content could:
-      // * be static content (here or in a shared file)
-      // * show the first page from the nav
-      // * be in the database but add code to hide in the nav
-      include(SHARED_PATH . '/static_homepage.php');
-    }
+      if(isset($page)) {
+        // show the page from the database
+        // TODO add html escaping back in
+        echo $page['content'];
+
+      } else {
+        // Show the homepage
+        // The homepage content could:
+        // * be static content (here or in a shared file)
+        // * show the first page from the nav
+        // * be in the database but add code to hide in the nav
+        include(SHARED_PATH . '/static_homepage.php');
+      }
     ?>
 
   </div>
